5b809fbe6f37b92514148d778f9d08f6e5291c5d
Boris Jelic <boris@Xs-MacBook-Pro.local>
2018-01-13 13:10:23 +1100

    changed the show function

--- main.go
+++ main.go
@@ -5,17 +5,22 @@ import (
 	"os"
 )
 
-func main() {
-	show := func(key string) {
-		val, ok := os.LookupEnv(key)
-		if !ok {
-			fmt.Printf("%s not set\n", key)
-		} else {
-			fmt.Printf("%s=%s\n", key, val)
-		}
+func show(key string) (string, bool) {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		fmt.Printf("%s not set\n", key)
+	} else {
+		fmt.Printf("%s=%s\n", key, val)
 	}
 
-	show("USER")
-	show("GOPATH")
+	return val, ok
+}
+
+func main() {
+	a, b := show("USER")
+
+	fmt.Println(a)
+	fmt.Println(b)
+	//show("PWD")
 
 }
